8d0aa9c12c9ca6d53043387c5ce625db3dbc1f4a
Darlene Ko <ko.darlene@gmail.com>
2020-05-29 09:13:11 -0500

    Push

--- assets/js/app.js
+++ assets/js/app.js
@@ -72,7 +72,7 @@ d3.csv("assets/data/data.csv").then(function(allData) {
     .attr("fill", "pink")
     .attr("opacity", ".5");
 
-    // Step 5 1/2: Create Text inside Circles
+    // Step 6: Create Text inside Circles
     // ==============================
     var textGroup = chartGroup.selectAll("chartGroup text")
     .data(allData)
@@ -83,7 +83,7 @@ d3.csv("assets/data/data.csv").then(function(allData) {
     .attr("y", d => yLinearScale(d.healthcare))
     .text(d => d.abbr);
 
-    // Step 6: Initialize tool tip
+    // Step 7: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
       .attr("class", "d3-tip")
@@ -92,13 +92,13 @@ d3.csv("assets/data/data.csv").then(function(allData) {
         return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
       });
 
-    // Step 7: Create tooltip in the chart
+    // Step 8: Create tooltip in the chart
     // ==============================
     chartGroup.call(toolTip);
 
-    // Step 8: Create event listeners to display and hide the tooltip
+    // Step 9: Create event listeners to display and hide the tooltip
     // ==============================
-    circlesGroup.on("click", function(data) {
+    circlesGroup.on("mouseover", function(data) {
       toolTip.show(data, this);
     })
       // onmouseout event
@@ -112,13 +112,13 @@ d3.csv("assets/data/data.csv").then(function(allData) {
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Healthcare");
+      .attr("class", "aText")
+      .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("Poverty");
+      .attr("class", "aText")
+      .text("In Poverty (%)");
   }).catch(function(error) {
     console.log(error);
   });
